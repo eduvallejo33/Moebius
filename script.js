2bbbc1b2e68e34cedfd9e6e3efb18a4088610862
eduvallejo33 <50503714+eduvallejo33@users.noreply.github.com>
2024-03-30 22:45:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,94 +31,14 @@ let params = {
 }
 
 let loader = new THREE.TextureLoader();
-let texture = loader.load('acapulco1.jpeg'); // Reemplaza 'path/to/your/image.jpg' por la ruta de tu imagen
+let texture = loader.load('acapulco1.jpeg'); // Reemplaza 'path/to/your/image.jpg' con la ruta de tu imagen
 
-let m = new THREE.MeshBasicMaterial({
+let m = new THREE.MeshStandardMaterial({
   map: texture, // Aplicar la textura al material
-  vertexColors: true,
-  onBeforeCompile: shader => {
-    shader.uniforms.time = gu.time;
-    shader.uniforms.instanceCount = params.instanceCount;
-    shader.uniforms.instanceLength = params.instanceLength;
-    shader.uniforms.instanceGap = params.instanceGap;
-    shader.uniforms.profileFactor = params.profileFactor;
-    shader.vertexShader = `
-      uniform float time;
-      
-      uniform float instanceCount;
-      uniform float instanceLength;
-      uniform float instanceGap;
-      
-      uniform float profileFactor;
-      
-      varying float noGrid;
-      
-      mat2 rot(float a){return mat2(cos(a), sin(a), -sin(a), cos(a));}
-      
-      ${shader.vertexShader}
-    `.replace(
-      `#include <begin_vertex>`,
-      `#include <begin_vertex>
-      
-        float t = time * 0.1;
-        
-        float iID = float(gl_InstanceID);
-        
-        float instanceTotalLength = instanceLength + instanceGap;
-        float instanceFactor = instanceLength / instanceTotalLength;
-        
-        float circleLength = instanceTotalLength * instanceCount;
-        float circleRadius = circleLength / PI2;
-        
-        float partAngle = PI2 / instanceCount;
-        float boxAngle = partAngle * instanceFactor;
-
-        float partTurn = PI / instanceCount;
-        float boxTurn = partTurn * instanceFactor;
-        
-        float startAngle = t + partAngle * iID;
-        float startTurn = t * 0.5 + partTurn * iID;
-        
-        float angleFactor = position.x;
-        
-        float angle = startAngle + boxAngle * angleFactor;
-        float turn = startTurn + boxTurn * angleFactor;
-        
-        vec3 pos = vec3(0, position.y, position.z);
-        pos.yz *= rot(turn);
-        pos.yz *= profileFactor;
-        pos.z += circleRadius;
-        pos.xz *= rot(angle);
-        
-        transformed = pos;
-        float nZ = floor(abs(normal.z) + 0.1);
-        float nX = floor(abs(normal.x) + 0.1);
-        noGrid = 1. - nX;
-        vColor = vec3(nZ == 1. ? 0.1 : nX == 1. ? 0. : 0.01);
-      `
-    );
-    shader.fragmentShader = `
-      varying float noGrid;
-      
-      float lines(vec2 coord, float thickness){
-        vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord) / thickness;
-        float line = min(grid.x, grid.y);
-        return 1.0 - min(line, 1.0);
-      }
-      ${shader.fragmentShader}
-    `.replace(
-      `#include <color_fragment>`,
-      `#include <color_fragment>
-        
-        float multiply = vColor.r > 0.05 ? 3. : 2.;
-        float edges = lines(vUv, 3.);
-        float grid = min(noGrid, lines(vUv * multiply, 1.));
-        diffuseColor.rgb = mix(diffuseColor.rgb, vec3(1), max(edges, grid));
-      `
-    )
-  }
+  color: 0xffffff, // Color base del material
+  roughness: 0.5, // Rugosidad del material
+  metalness: 0.5 // Metalicidad del material
 });
-m.defines = { "USE_UV": "" };
 
 let o = new THREE.Mesh(ig, m);
 scene.add(o)
